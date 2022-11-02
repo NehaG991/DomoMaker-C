1ade20355186f584dca5e2012ae322632e2ba016
Neha Ghanta <55118296+NehaG991@users.noreply.github.com>
2022-11-01 20:52:59 -0400

    finished assignment

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const helmet = require('helmet');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const redis = require('redis');
+const csrf = require('csurf');
 
 const router = require('./router.js');
 
@@ -27,7 +28,7 @@ const redisURL = process.env.REDISCLOUD_URL
   || 'redis://default:OtTxSFzae5ALUkwSNi9SywxtOOxTFRaC@redis-15951.c11.us-east-1-3.ec2.cloud.redislabs.com:15951';
 
 const redisClient = redis.createClient({
-  legacyMode: true, 
+  legacyMode: true,
   url: redisURL,
 });
 redisClient.connect().catch(console.error);
@@ -59,6 +60,14 @@ app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 
 app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token!');
+  return false;
+});
 
 router(app);
 
